3fbbddc1a08b7062e07500f213e25b47f2016389
Adam Kaput <adamkaput@gmail.com>
2016-03-01 15:43:56 +0100

    Gruntfile updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,8 @@ module.exports = function (grunt) {
                     'src/<%= pkg.name %>.js',
                     'src/auth/LoginSession.js',
                     'src/auth/User.js',
-                    'src/auth/auth.js'
+                    'src/auth/auth.js',
+                    'src/upload/upload.js'
                 ],
                 dest: 'dist/<%= pkg.name %>.js'
             },
